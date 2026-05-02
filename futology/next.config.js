7aaefc10f4784549b004e64f6ad5121ac4792033
Sonik Krish <sonikrish2248@gmail.com>
2026-05-02 13:46:59 +0530

    feat(build): static export config gated by NEXT_OUTPUT, basePath via env

--- futology/next.config.js
+++ futology/next.config.js
@@ -1,13 +1,25 @@
 /** @type {import('next').NextConfig} */
+
+const isExport = process.env.NEXT_OUTPUT === "export";
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "media.api-sports.io" },
       { protocol: "https", hostname: "crests.football-data.org" },
       { protocol: "https", hostname: "img.a.transfermarkt.technology" },
     ],
   },
+  ...(isExport
+    ? {
+        output: "export",
+        trailingSlash: true,
+        ...(basePath ? { basePath, assetPrefix: `${basePath}/` } : {}),
+      }
+    : {}),
 };
 
 module.exports = nextConfig;
