4f71de6ba3f3e274302eacad2a718b1601e3cb3a
Sonik Krish <sonikrish2248@gmail.com>
2026-05-09 23:10:19 +0530

    feat: add loading state for profile page

---
+++ futology/app/profile/loading.tsx
@@ -0,0 +1,9 @@
+import { ProfileSkeleton } from "@/components/shared/ProfileSkeleton";
+
+export default function Loading() {
+  return (
+    <div className="container-page py-6">
+      <ProfileSkeleton />
+    </div>
+  );
+}
